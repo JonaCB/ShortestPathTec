3d872b0d41df9639fc3c0ef03f3228f5af4e8f86
Jona Cabrera <jonacabrerab@hotmail.com>
2018-04-23 16:31:34 -0500

    Frame

--- src/ShortestPathTec.java
+++ src/ShortestPathTec.java
@@ -1,10 +1,14 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class ShortestPathTec extends JFrame{
 	
 	public static void main(String[] args) {
-		JFrame window = new JFrame();
+		JFrame window = new JFrame("¡Bienvenido!");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.setPreferredSize(new Dimension(640, 480));
 	}
 
 }
