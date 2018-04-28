f413b5426e0ed8369b343db67d070575f4c8d2b9
Jona Cabrera <jonacabrerab@hotmail.com>
2018-04-28 15:18:29 -0500

    Arre la que barre

--- src/PanelControl.java
+++ src/PanelControl.java
@@ -2,6 +2,7 @@ import java.awt.Dimension;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -11,16 +12,19 @@ public class PanelControl extends JPanel{
 	private PanelDibujo pd;
 	private JComboBox<String> lstFrom, lstTo;
 	private JButton btnGo;
-	private static BufferedReader bf = null;
+	private String[] nombres;
+	//private static BufferedReader bf = null;
 	
 	//Metodo para leer los nombres de los lugares para los combobox
-	public static void readNames() {
-		//File nombres = new File();
+	public void readNames() {
+		BufferedReader bf = null;
+		int i = 0;
 		try {
-			bf = new BufferedReader(new FileReader("Proyecto_Lugares - Hoja 1.csv"));
+			bf = new BufferedReader(new FileReader("src\\Proyecto_ Lugares - Hoja 1.csv"));
 			String line;
 			while ((line = bf.readLine()) != null) {
-		        System.out.println(line);
+		        nombres[i] = line;
+		        i++;
 		    }
 		}
 		catch(IOException e) {
@@ -29,6 +33,7 @@ public class PanelControl extends JPanel{
 		finally {
 			 try {
 			        bf.close();
+			        System.out.println("arre la que barre");
 			    } catch (IOException e) {
 			        e.printStackTrace();
 			    }
@@ -38,10 +43,26 @@ public class PanelControl extends JPanel{
 	public PanelControl() {
 		super();
 		this.setPreferredSize(new Dimension(260, 719));
+		this.nombres = new String[54];
+		this.readNames();
+		lstFrom = new JComboBox<String>(nombres);
+		this.add(lstFrom);
 		
 	}
 	
-	public static void main(String[] args) {
-		readNames();
+	public String[] getNombres() {
+		return this.nombres;
 	}
+	
+	/*public static void main(String[] args) {
+		PanelControl pc = new PanelControl();
+		pc.nombres = new String[54];
+		pc.readNames();
+		System.out.println(pc.getNombres().length);
+		int i = 0;
+		for(String a:pc.getNombres()) {
+			System.out.println(""+i+" "+a);
+			i++;
+		}
+	}*/
 }
